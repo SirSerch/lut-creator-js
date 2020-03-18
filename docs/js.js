f3cbe09a5d60afa8c85541ebed09caadcf2732cb
Sergio Santos <santoscamin@gmail.com>
2020-03-18 23:23:38 +0100

    docs

---
+++ docs/js.js
@@ -0,0 +1,21 @@
+console.log('%c Con que te gusta investigar...( ͡° ͜ʖ ͡°)', 'font-size: 15px; color: white; background: black')
+
+function getRadius(lutSize) {
+    let radius = document.getElementsByName("lutSize");
+    if (radius[2].checked)
+        lutSize = 144;
+    else if (radius[1].checked)
+        lutSize = 64;
+    else
+        lutSize = 25;
+    return lutSize;
+}
+
+function generateHald() {
+    let hald = new Hald(getRadius());
+    hald.createHald();
+}
+
+function generateCube() {
+    Hald.exportCube();
+}
