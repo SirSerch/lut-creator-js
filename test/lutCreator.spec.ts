f3a414428f9fac340efef2e8387da8a8264c0e6f
Kristian Muñoz <kristian.mzmz@gmail.com>
2023-06-27 09:03:28 +0200

    Refactor tests to have a easier way to generate canvas by LUT size

--- test/lutCreator.spec.ts
+++ test/lutCreator.spec.ts
@@ -1,27 +1,35 @@
 import {Hald} from "../Hald";
 
+
 describe('Lut Creator', () => {
     describe('Create Hal should generate a png file', () => {
         it('should create a HAL png file with canvas size zero if LUT size is zero', () => {
-            const canvasZero = document.createElement('canvas');
-            canvasZero.width = 0;
-            canvasZero.height = 0;
-            const hald = new Hald(0);
+            let lutSize = 0;
+            const canvasZero = aCanvasWithSize(lutSize);
+            const hald = new Hald(lutSize);
             const spyExportHald = jest.spyOn(hald as any,'exportHald')
 
             expect(() => hald.createHald(0)).not.toThrowError();
-            expect(spyExportHald).toHaveBeenCalledWith(canvasZero, 0);
+            expect(spyExportHald).toHaveBeenCalledWith(canvasZero, lutSize);
         });
 
         it('should create the png file if lut size is not zero', () => {
-            const aCanvasWithCalculatedSize = document.createElement('canvas');
-            aCanvasWithCalculatedSize.width = 89;
-            aCanvasWithCalculatedSize.height = 89;
-            const hald = new Hald(20);
+            let lutSize = 20;
+            const aCanvasWithCalculatedSize = aCanvasWithSize(lutSize);
+            const hald = new Hald(lutSize);
             const spyExportHald = jest.spyOn(hald as any,'exportHald')
 
-            expect(() => hald.createHald(20)).not.toThrowError();
-            expect(spyExportHald).toHaveBeenCalledWith(aCanvasWithCalculatedSize, 20);
+            expect(() => hald.createHald(lutSize)).not.toThrowError();
+            expect(spyExportHald).toHaveBeenCalledWith(aCanvasWithCalculatedSize, lutSize);
         });
     });
 });
+
+// TODO move this logic to somewhere once the production code allows it
+function aCanvasWithSize(lutSize: number) {
+    const canvasSize = Math.round((lutSize ** 3) ** 0.5);
+    const aCanvasWithCalculatedSize = document.createElement('canvas');
+    aCanvasWithCalculatedSize.width = canvasSize;
+    aCanvasWithCalculatedSize.height = canvasSize;
+    return aCanvasWithCalculatedSize;
+}
